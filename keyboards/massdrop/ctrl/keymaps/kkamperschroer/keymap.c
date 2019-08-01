6cd5fb8b5aebfc194d3365270391443125da052b
Kyle Kamperschroer <kkamperschroer@gmail.com>
2019-07-31 21:09:34 -0500

    Set initial hue to orange/red when initialized

--- keyboards/massdrop/ctrl/keymaps/kkamperschroer/keymap.c
+++ keyboards/massdrop/ctrl/keymaps/kkamperschroer/keymap.c
@@ -45,6 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // Runs just one time when the keyboard initializes.
 void matrix_init_user(void) {
+    rgb_matrix_sethsv(20,  UINT8_MAX, 127);
 };
 
 // Runs constantly in the background, in a loop.
